6cf59dd84844a7d20815a470fbe4ba4353bc5d51
mleibeng <mleibeng@student.42.fr>
2024-12-17 02:45:18 +0000

    Ideas to start

--- Day5/Ex01/main.go
+++ Day5/Ex01/main.go
@@ -15,5 +15,11 @@ func main() {
 
 	readout := csv.NewReader(file)
 
-
+	// Idea to start:
+	// * index each number,
+	// * check if the index ordering related to the first part of the csv is in correct order for the printing orders in the below instructions
+	// * count the amount of correct instruction lines
+	// * enter at instruction of %2 of amount numbers of instruction line
+	// * read out value and add to totalsum
+	// * print totalsum
 }
